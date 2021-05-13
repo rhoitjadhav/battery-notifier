357b5a22163956b3b54341124b019f71feb67d91
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-13 19:55:12 +0530

    project dependency installation script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Install Python3
+sudo apt install python3
+
+# Install notify2
+sudo apt install python3-notify2
+
+# Install psutil
+sudo apt install python3-psutil

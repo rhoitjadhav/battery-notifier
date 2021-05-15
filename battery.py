b64d0975262aea5d5602c28fc171db86cc257912
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-15 14:13:04 +0530

    create _sensors_battery() method

--- battery.py
+++ battery.py
@@ -2,17 +2,18 @@ import psutil
 
 
 class Battery:
-    def __init__(self) -> None:
-        self._battery = psutil.sensors_battery()
+    @staticmethod
+    def _sensors_battery():
+        return psutil.sensors_battery()
 
     def get_percentage(self) -> int:
         """
         Returns battery percentage
         """
-        return self._battery.percent
+        return self._sensors_battery().percent
 
     def is_plugged(self) -> bool:
         """
         Returns boolean whether the battery power plugged or not
         """
-        return self._battery.power_plugged
+        return self._sensors_battery().power_plugged

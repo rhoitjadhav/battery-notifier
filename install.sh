4c62ecd0073191210c0be6bae63dc78f36db12a2
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-15 15:06:26 +0530

    installation script

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@ set -e
 # Install Python3
 sudo apt install python3
 
-# Install notify2
-sudo apt install python3-notify2
+# dbus-python package dependencies
+sudo apt install build-essential libpython3-dev libdbus-1-dev libdbus-glib-1-dev libgirepository1.0-dev
 
 # Install psutil
 sudo apt install python3-psutil
+
+# Install notify2
+sudo apt install python3-notify2

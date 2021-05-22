f4259ddc67d8f7d837c437284362549f100c986b
rhoitjadhav <jadhavrohit497@gmail.com>
2021-05-22 21:34:15 +0530

    creating Notification class object from init, added docstring

--- notifier.py
+++ notifier.py
@@ -1,18 +1,35 @@
+# Packages
 import notify2
+from notify2 import Notification
 
 
 class Notifier:
-    def __init__(self, app_name: str) -> None:
-        notify2.init(app_name)
+    """
+    Notifier class is responsible for creating and managing the
+    notification
+    Args:
+        summary: title of the notification
+        message: message to be displayed
+        icon: icon of notification bar
+    """
 
-    @staticmethod
-    def show(summary: str, message: str = "", icon: str = "") -> None:
+    def __init__(self, app_name: str, summary: str, message: str = "", icon: str = "") -> None:
+        self._app_name = app_name
+        self._summary = summary
+        self._message = message
+        self._icon = icon
+
+        notify2.init(self._app_name)
+        self._notification = Notification(self._summary, self._message, self._icon)
+
+    def show(self) -> None:
         """
         Show notification bar
-        Args:
-            summary: title of the notification
-            message: message to be displayed
-            icon: icon of notification bar
         """
-        notification = notify2.Notification(summary, message, icon)
-        notification.show()
+        self._notification.show()
+
+    def close(self) -> None:
+        """
+        Remove notification bar from tray
+        """
+        self._notification.close()
